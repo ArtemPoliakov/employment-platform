f8199e12cf346c67c9eb05024990d608684fe6ef
ArtemPoliakov <apoliakov50@gmail.com>
2025-05-11 10:33:20 +0300

    first dependencies added, router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Outlet,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+
 function App() {
-  return (
-    <>
-      <div>Hello world!</div>
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<h1>Home</h1>} />
+        <Route path="about" element={<h1>About</h1>} />
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
+
+const Root = () => {
+  return <Outlet />;
+};
 
 export default App;
